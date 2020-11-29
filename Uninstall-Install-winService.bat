daa7de2cec7a2b0672fb5c977dd6dde27e4b8675
Miuroku <thebestguy1101001@gmail.com>
2020-11-30 01:55:12 +0300

    delete trash

--- Uninstall-Install-winService.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
-InstallUtil.exe -u C:\Users\supay\Desktop\Labs_CS\Windows-service-test\FileWatcherForFun\FileWatcherForFun\bin\Debug\FileWatcherForFun.exe
-InstallUtil.exe C:\Users\supay\Desktop\Labs_CS\Windows-service-test\FileWatcherForFun\FileWatcherForFun\bin\Debug\FileWatcherForFun.exe
-pause
